76ae1e2ee28369d5b72c1b1122f9c79b06a4bcfc
Faizan0147 <faizan228918@gmail.com>
2024-04-21 02:37:10 +0500

    Updated Card.java
    Completed the code of Card class

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,129 @@
+
 public class Card {
 
+    private int id;
+    private String title;
+    private String description;
+    private String status;
+    private String label;
+    private boolean blocked;
+    private boolean urgent;
+    private Date date;
+    private Date dateCreated;
+    private ArrayList<Member> assigned;
+
+
+
+
+    public Card(int id, String title, String description, String status,
+                String label, boolean blocked, boolean urgent, Date date) {
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.label = label;
+        this.blocked = blocked;
+        this.urgent = urgent;
+        this.date = date;
+        this.dateCreated = new Date(); // Automatically set to current creation date
+        this.assigned = new ArrayList<>();
+    }
+
+
+
+
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public int getID(int id){
+        return id;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(String tile){
+        return title;
+
+    }
+
+    public void setDescription(String Description){
+        this.description = description;
+    }
+
+    public String getDescription(String description){
+        return description;
+    }
+
+    public void setStatus(String status){
+        this.status= status;
+    }
+
+    public String getStatus(String status){
+        return status;
+
+    }
+
+    public void setLabel(String label){
+        this.label= label;
+    }
+
+    public String getLabel(String label){
+        return label;
+
+    }
+
+    public void setBlocked (boolean blocked){
+        this.blocked = blocked;
+    }
+
+    public boolean getBlocked(boolean blocked){
+        return blocked;
+    }
+
+    public void setUrgent (boolean urgent){
+        this.urgent= urgent;
+    }
+
+    public boolean getUrgent(boolean urgent){
+        return urgent;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public Date getDate(Date date){
+        return date;
+    }
+    public void setDateCreated(Date dateCreated){
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateCreated(Date dateCreated){
+        return dateCreated;
+    }
+
+
+    public void addMember(Member member) {
+        assigned.add(member);
+    }
+
+    public void deleteMember(int index) {
+        if (assigned == null || index < 0 || index >= assigned.size()) {
+            return;  // for invalid index
+        }
+        assigned.remove(index);
+    }
+
+    public ArrayList<Member> getMembers() {
+
+        return assigned;
+    }
+
 }
+
