63a68585745d62789fe2630a81ac509d5884f54c
Darker98 <96902917+Darker98@users.noreply.github.com>
2024-04-29 10:15:08 +0500

    Finished class definitions
    Completed the following classes:
    - Manager
    - Member
    - Swimlane
    - User

--- src/Swimlane.java
+++ src/Swimlane.java
@@ -1,3 +1,23 @@
 public class Swimlane {
+    private int id;
+    private String label;
+    
+    Swimlane(String label) {
+        this.label = label;
+    }
 
+    // Getter for id
+    public int getID() {
+        return id;
+    }
+
+    // Getter for label
+    public String getLabel() {
+        return label;
+    }
+
+    // Setter for label
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
