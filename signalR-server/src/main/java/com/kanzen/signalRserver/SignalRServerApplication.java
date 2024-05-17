e31ae8e990b0a0880ab29b74d05e878dd1cd3f26
Darker98 <96902917+Darker98@users.noreply.github.com>
2024-05-17 19:58:11 +0500

    Added SignalR Project

---
+++ signalR-server/src/main/java/com/kanzen/signalRserver/SignalRServerApplication.java
@@ -0,0 +1,13 @@
+package com.kanzen.signalRserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalRServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalRServerApplication.class, args);
+	}
+
+}
---
+++ signalR-server/src/test/java/com/kanzen/signalRserver/SignalRServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanzen.signalRserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalRServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
